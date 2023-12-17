0e17291443769471b54875525df7b1e58c16666a
Debmalya Biswas <viperbale.db@gmail.com>
2023-12-18 02:44:27 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+const page = () => {
+  return (
+    <div className="home-wrapper">
+      <div className="home-container">
+        <div className="home-content">
+          I am Home
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
