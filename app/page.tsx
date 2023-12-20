cbf4ec2d111793fca42819d8d5c3402f5b9e5f1e
Debmalya Biswas <viperbale.db@gmail.com>
2023-12-21 01:49:26 +0530

    socket init

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './style.scss'
-const page = () => {
+"use client";
+import React, { useEffect } from "react";
+import "./style.scss";
+import io from "socket.io-client";
+const Home = () => {
+  useEffect(() => {
+    const socket = io("http://localhost:8080");
+    socket.on("connect", () => {
+      console.log("Connected to Socket.io");
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <div className="home-wrapper">
       <div className="home-container">
-        <div className="home-content">
-          I am Home
-        </div>
+        <div className="home-content">I am Home</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Home;
---
+++ components/Socket/index.ts
@@ -0,0 +1,20 @@
+// import io from "socket.io-client";
+
+// const socket = io("http://localhost:4000");
+
+
+// socket.on("connect", () => {
+//   console.log("Connected to server!");
+// });
+
+// socket.on("message", (data: string) => {
+//   console.log("Received message:", data);
+// });
+
+
+// socket.emit("sendMessage", "Hello, server!");
+
+
+// socket.on("disconnect", () => {
+//   console.log("Disconnected from server!");
+// });
